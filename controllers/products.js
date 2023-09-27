1c053eca521c59da4e1ba45564941a6985ffa547
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-09-27 18:16:28 +0300

    limit and pagging the products

--- controllers/products.js
+++ controllers/products.js
@@ -35,6 +35,12 @@ exports.getAllProducts = async (req, res, next) => {
     }
 
 
+    // Limit and Pages  
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result.limit(limit).skip(skip);
+
     const products = await result;
     res.status(200).json({ products });
 }
